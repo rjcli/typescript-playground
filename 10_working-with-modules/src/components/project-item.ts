628f5c0267deef5695e94fb5c45f1d5df439a242
Rashmi Raj <rashmi.raj@intel.com>
2024-09-23 11:22:00 +0530

    Adds chapter 10: Working with Modules

---
+++ 10_working-with-modules/src/components/project-item.ts
@@ -0,0 +1,51 @@
+import { autobind as Autobind } from '../decorators/autobind.js';
+import { Draggable } from '../models/drag-drop.js';
+import { Project } from '../models/project.js';
+import Component from './base-component.js';
+
+// Project Item Class
+class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  @Autobind
+  dragStartHandler(event: DragEvent): void {
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
+  }
+
+  dragEndHandler(_: DragEvent): void {
+    console.log('DragEnd');
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
+export default ProjectItem;
