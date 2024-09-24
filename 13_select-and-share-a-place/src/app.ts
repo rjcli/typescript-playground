b364e2af564e417afaa8c628f99d146e2c37317a
Rashmi Raj <rashmi.raj@intel.com>
2024-09-24 11:38:13 +0530

    Adds chapter 13: Select and Share a place

---
+++ 13_select-and-share-a-place/src/app.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const formEl = document.querySelector('form')! as HTMLFormElement;
+const addressInputEl = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHuowXOs';
+
+type GoogleGeocodingResponse = {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault();
+  const enteredAddress: string = addressInputEl.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`,
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location...');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        },
+      );
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+};
+
+formEl.addEventListener('submit', searchAddressHandler);
